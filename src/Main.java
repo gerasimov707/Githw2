6c2a99ce1c70314e9525ce24a31d54be86e013fc
Gerasimov Alexandr <gerasimov707@yandex.ru>
2022-12-26 19:02:32 +0300

    сделал коммит

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
 public class Main {
 
-    public class SalesManager {
-        protected int[] sales;
-
-        public SalesManager(int[] sales) {
-            this.sales = sales;
-        }
-
-        public int max() {
-            int max = -1;
-            for (int sale : sales) {
-                if (sale > max) {
-                    max = sale;
-                }
-            }
-            return max;
-        }
+    public static void main(String[] args) {
     }
 }
