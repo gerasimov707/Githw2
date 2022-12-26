5b1ac320a94fcaf39324d65ad2d393f4d4edfd92
Gerasimov Alexandr <gerasimov707@yandex.ru>
2022-12-26 21:12:14 +0300

    сделал коммит 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         int[] sales = {100,55,900,200};
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println("самая крупная продажа " + salesManager.max() + " долларов ");
+        System.out.println("самая крупная продажа " + salesManager.max() + " долларов  ");
     }
 }
