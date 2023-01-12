fc3a70df00795307a98c1d5aa862dea2d69096f6
Gerasimov Alexandr <gerasimov707@yandex.ru>
2023-01-12 18:37:46 +0300

    сделал коммит long int

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] sales = {100,55,900,200};
+        long[] sales = {100,0,900,200};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println("самая крупная продажа " + salesManager.max() + " долларов ");
+
+
+        }
     }
-}
+
